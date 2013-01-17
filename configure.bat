86516f3ad53817787511d09341c94411a8e26f9d
Matthias Ableitner <matthias.ableitner@swcz.de>
2013-01-17 18:05:15 +0100

    use env vars for paths

--- configure.bat
+++ configure.bat
@@ -1,4 +1,4 @@
 @echo off
 del CMakeCache.txt
-set PATH=%PATH%;C:\MinGW32\bin\;C:\Programme\CMake 2.8\bin\;C:\Programme\Ninja
+set PATH=%PATH%;C:\MinGW32\bin\;%ProgramFiles%\CMake 2.8\bin\;%ProgramFiles%\Ninja;%ProgramFiles%\Git\bin
 cmake.exe -G Ninja .
